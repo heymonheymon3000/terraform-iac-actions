6b0f762afb25b3c08d53a6f764037253010d509f
heymonheymon3000 <heymonheymon3000@yahoo.com>
2026-03-28 12:20:17 -0400

    fixing s3 bucket issue

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -24,7 +24,8 @@ terraform {
     region = "us-east-1"
 
     # For State Locking
-    dynamodb_table = "dev-ekscluster"
+    # dynamodb_table = "dev-ekscluster"
+    use_lockfile   = true # Use S3 native locking
   }
 }
 
